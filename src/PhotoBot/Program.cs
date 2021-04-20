3696b3ba7117bffb733878f32c4ad208887325c3
Lennard Beers <lennard.beers@rub.de>
2021-04-20 23:09:33 +0200

    created project

---
+++ src/PhotoBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PhotoBot
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
